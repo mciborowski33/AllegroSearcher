05e058731060dc592b396509fd21e55592a15911
Paulina <inaszkola@gmail.com>
2019-11-17 21:38:08 +0100

    interfejs druga część

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 window.onload = function() {
   //alert("JS działa.")
+
+  $("#d2").hide();
+  $("#searchProducts").click(function(){
+    $("#d1").hide();
+    $("#d2").show();
+  });
+  $("#back").click(function(){
+    $("#d2").hide();
+    $("#d1").show();
+  });
+
 };
