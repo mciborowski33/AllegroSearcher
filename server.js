8be53884450b61ac9df3b106723e588b97544477
Michal Ciborowski <mciborowski33@gmail.com>
2019-11-17 16:27:58 +0100

    allegro desperacka próba

--- server.js
+++ server.js
@@ -11,22 +11,29 @@ app.use('/scripts', express.static('scripts'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
+app.get('/allegroCode.html', (req, res) => res.sendFile(__dirname + '/allegroCode.html'));
+
 app.get("/allegro", function(request, response) {
-  var proxyRequest = http.request({
-      host: "https://allegro.pl.allegrosandbox.pl/auth/oauth/token?grant_type=client_credentials",
-      port: 8000,
-      method: 'GET',
-      path: '/',
-      username: 'a16004d41096431198bc9baf3d58aa0c',
-      password: 'ADTNwyWVNJKQ8YpJR4G4svF0mAA0aosD8FnNzcqgsRvquLU9X4fPdVczPjSvJBwq'
-    },
+    response.redirect('https://allegro.pl/auth/oauth/authorize?response_type=code&client_id=a16004d41096431198bc9baf3d58aa0c&redirect_uri=http://localhost/allegroCode.html')
+    }
+/*
+    var options = {
+        host: "https://allegro.pl/auth/oauth/authorize?response_type=code&client_id=a16004d41096431198bc9baf3d58aa0c&redirect_uri=http://localhost/allegro",
+        port: 80,
+        method: 'GET',
+        path: '/',
+        //username: 'a16004d41096431198bc9baf3d58aa0c',
+        //password: 'ADTNwyWVNJKQ8YpJR4G4svF0mAA0aosD8FnNzcqgsRvquLU9X4fPdVczPjSvJBwq'
+    };
+
+  var proxyRequest = http.request( options,
     function (proxyResponse) {
       proxyResponse.on('data', function (chunk) {
         //response.send(chunk);
         alert(chunk);
       });
     });
-
+*/
   //proxyRequest.write(response.body);
   //proxyRequest.end();
-});
+);
