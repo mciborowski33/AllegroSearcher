9ab9b37d253c3a1959f9418294af27d08fd27501
Paulina <inaszkola@gmail.com>
2019-11-18 17:55:47 +0100

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,47 @@
 window.onload = function() {
   //alert("JS działa.")
 
-  $("#d2").hide();
-  $("#searchProducts").click(function(){
-    $("#d1").hide();
-    $("#d2").show();
-  });
-  $("#back").click(function(){
-    $("#d2").hide();
-    $("#d1").show();
-  });
-
+  document.getElementById('d2').style.display="none";
+  document.getElementById('d1').style.display="block";
 };
+var a = 0;
+
+function hide(){
+  if(a>0){
+    var x = document.getElementById('d1');
+    var y = document.getElementById('d2');
+    if(x.style.display === "block"){
+      x.style.display="none";
+      y.style.display="block";
+    }else{
+      x.style.display="block";
+      y.style.display="none";
+    }
+  }
+}
+
+function appendToList(){
+  var name = document.getElementById('productName').value;
+  var c_min = document.getElementById('cmin').value;
+  var c_max = document.getElementById('cmax').value;
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(name);
+  var closeBtn = document.createElement("BUTTON");
+  closeBtn.innerHTML = "&times;";
+  closeBtn.classList.add("close");
+  closeBtn.setAttribute("id", "closeBtn");
+  closeBtn.setAttribute("onclick", "removeFromList()");
+  node.classList.add("list-group-item");
+  if(a<5){
+    a++;
+    node.appendChild(textnode);
+    node.appendChild(closeBtn);
+    document.getElementById('productList').appendChild(node);
+  }
+}
+function removeFromList(){
+  a--;
+  var target = event.target;
+  var id = target.parentNode.id;
+  document.getElementById('productList').removeChild(target.parentNode);
+}
