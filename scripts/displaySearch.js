fd6e8a6cfcfb46fd70e45315fc618c61986b288c
Paulina <inaszkola@gmail.com>
2019-11-25 14:24:40 +0100

    display 2

--- scripts/displaySearch.js
+++ scripts/displaySearch.js
@@ -6,15 +6,22 @@ var socket = io.connect('http://localhost');
 });
 
 function display(){
-  document.getElementById('r1').innerHTML = "Zestaw 1 <span>Cena</span>";
   var name = document.getElementById('productName').value;
-  var price;
+  var price = "Cena";
+  var prodPrice = "Price";
+  var sp1 = document.createElement("SPAN");
+  var sp2 = document.createElement("SPAN");
   var node = document.createElement("A");
-  var textnode = document.createTextNode(name);
-  node.classList.add("list-group-item");
-  node.appendChild(textnode);
+  var title = document.getElementById('r1');
+  sp1.classList.add("font-weight-bold");
+  sp1.innerHTML = price;
+  sp2.style= "float:right";
+  sp2.innerHTML = prodPrice;
+  title.innerHTML = "Zestaw 1 - ";
+  title.appendChild(sp1);
+  node.innerHTML = name;
+  node.appendChild(sp2);
   node.setAttribute("href", "#");
   node.classList.add("list-group-item", "list-group-item-action");
   document.getElementById('resultList1').appendChild(node);
-
 }
