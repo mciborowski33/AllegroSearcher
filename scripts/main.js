45408a8859344f2e6fe08e941612ef5d48ec31ff
Paulina <inaszkola@gmail.com>
2019-11-26 21:41:45 +0100

    znaki zabronione

--- scripts/main.js
+++ scripts/main.js
@@ -99,3 +99,19 @@ document.getElementById('searchProducts').addEventListener("click", function(){
     //console.log(JSON.stringify(product_array));
     sendSearch(JSON.stringify({searchData: product_array}));
 });
+
+function isNumberKey(evt){
+  var charCode = (evt.which) ? evt.which : evt.keyCode;
+  if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)){
+	   return false;
+  }
+	return true;
+}
+
+function isAllowedKey(evt){
+  var charCode = (evt.witch) ? evt.which : evt.keyCode;
+  if(charCode < 31 || charCode == 34 || charCode == 47 || charCode == 58 || charCode == 91 || charCode == 92 || charCode == 93 || charCode == 123 || charCode == 125){
+    return false;
+  }
+  return true;
+}
