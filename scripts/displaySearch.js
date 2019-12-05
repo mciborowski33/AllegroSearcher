62d1fddfafb9ff9ffad5d3f223c5ab288013005a
Paulina <inaszkola@gmail.com>
2019-12-05 17:41:08 +0100

    ceny

--- scripts/displaySearch.js
+++ scripts/displaySearch.js
@@ -12,19 +12,20 @@ function display(result){
   var listLength = product_array.length;
   console.log(listLength);
   for(i=0; i<3; i++){
-    var price = 0;
-    for(j=0; j<listLength; j++){
-    var product = result[i][j];
-    price = price + product.price;
+    var price = parseFloat(0);
     var sp = document.createElement("SPAN");
-    var id = 'r' + (i+1);
-    var title = document.getElementById(id);
+    for(j=0; j<listLength; j++){
+      var product = result[i][j];
+      price = price + parseFloat(product.price);
+      var id = 'r' + (i+1);
+      var title = document.getElementById(id);
+      displayItem(product, i);
+    }
+    price = parseFloat(price).toFixed(2);
     sp.classList.add("font-weight-bold");
     sp.innerHTML = price + "zł";
     title.innerHTML = "Zestaw " + (i+1).toString() + " - ";
     title.appendChild(sp);
-    displayItem(product, i);
-    }
   }
 }
 
